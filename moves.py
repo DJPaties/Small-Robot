93141506067116e6c2bd676a32e39c34986d6f25
genixRobot <dghaily725@gmail.com>
2024-05-10 15:38:31 +0300

    Update moves.py

--- moves.py
+++ moves.py
@@ -1,22 +1,22 @@
-import wiringpi as wp
+import subprocess
 import time
 
-# Set GPIO pin numbering mode
-wp.wiringPiSetupGpio()
-
-# Define LED pin
-LED_PIN = 27
-
-# Set pin mode to output
-wp.pinMode(LED_PIN, wp.OUTPUT)
+# Define GPIO pin and state
+GPIO_PIN = 27
+STATE_ON = "1"
+STATE_OFF = "0"
+subprocess.run(["gpio", "mode", str(GPIO_PIN), "out"])
 
 try:
     while True:
-        # Toggle LED
-        wp.digitalWrite(LED_PIN, wp.HIGH)
+        # Turn on GPIO pin
+        subprocess.run(["gpio", "write", str(GPIO_PIN), STATE_ON])
         time.sleep(1)
-        wp.digitalWrite(LED_PIN, wp.LOW)
+        
+        # Turn off GPIO pin
+        subprocess.run(["gpio", "write", str(GPIO_PIN), STATE_OFF])
         time.sleep(1)
+
 except KeyboardInterrupt:
     # Clean up
-    wp.pinMode(LED_PIN, wp.INPUT)
+    subprocess.run(["gpio", "write", str(GPIO_PIN), STATE_OFF])
