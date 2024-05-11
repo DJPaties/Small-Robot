a85951204f564232cac2f73060c886dce1212090
genixRobot <dghaily725@gmail.com>
2024-05-11 13:00:49 +0300

    Update test.py

--- test.py
+++ test.py
@@ -36,102 +36,45 @@ def random_movement():
 #         return None
     
 
-# def stt(languageCode):
-#     lang_code = languageCode
-#     # Create a recognizer object
-#     r = sr.Recognizer()
+def stt(languageCode):
+    lang_code = languageCode
+    # Create a recognizer object
+    r = sr.Recognizer()
 
-#     # Open the microphone for capturing the speech
-#     with sr.Microphone() as source:
-#         print("Listening...")   
+    # Open the microphone for capturing the speech
+    with sr.Microphone() as source:
+        print("Listening...")   
         
-#         # Adjust the energy threshold for silence detection
-#         r.energy_threshold = 4000
+        # Adjust the energy threshold for silence detection
+        r.energy_threshold = 4000
 
-#         # Listen for speech and convert it to text
-#         audio = r.listen(source)
+        # Listen for speech and convert it to text
+        audio = r.listen(source)
 
-#         try:
+        try:
 
-#             text = r.recognize_google(audio, language=lang_code)
-#             print("You said:", text)
+            text = r.recognize_google(audio, language=lang_code)
+            print("You said:", text)
 
         
-#         except sr.UnknownValueError:
-#             x = 'Not Understand'
-#             # x = stt(languageCode)
-#             return x
+        except sr.UnknownValueError:
+            x = 'Not Understand'
+            # x = stt(languageCode)
+            return x
 
             
-#         except sr.RequestError as e:
-#             text="Could not request results from Google Speech Recognition service; {0}".format(e)
-#             print(text)
-#             # self.open_mic()
-#         return text
-    
-# Python program to translate
-# speech to text and text to speech
-
-
-import speech_recognition as sr
-import pyttsx3 
-
-# Initialize the recognizer 
-r = sr.Recognizer() 
-
-# Function to convert text to
-# speech
-def SpeakText(command):
-	
-	# Initialize the engine
-	engine = pyttsx3.init()
-	engine.say(command) 
-	engine.runAndWait()
-	
-	
-# Loop infinitely for user to
-# speak
-def stt():
-# while(1): 
-	
-	# Exception handling to handle
-	# exceptions at the runtime
-	try:
-		
-		# use the microphone as source for input.
-		with sr.Microphone() as source2:
-			
-			# wait for a second to let the recognizer
-			# adjust the energy threshold based on
-			# the surrounding noise level 
-			r.adjust_for_ambient_noise(source2, duration=0.2)
-			
-			#listens for the user's input 
-			audio2 = r.listen(source2)
-			
-			# Using google to recognize audio
-			MyText = r.recognize_google(audio2)
-			MyText = MyText.lower()
-
-			print("Did you say ",MyText)
-			SpeakText(MyText)
-			
-	except sr.RequestError as e:
-		print("Could not request results; {0}".format(e))
-		
-	except sr.UnknownValueError:
-		print("unknown error occurred")
+        except sr.RequestError as e:
+            text="Could not request results from Google Speech Recognition service; {0}".format(e)
+            print(text)
+            # self.open_mic()
+        return text
     
     
-
-
-
-
 try:
     while True:
         # Accept user input via voice command
         # command = listen_for_command()
-        command = stt()
+        command = stt("en")
 
         # Check various conditions based on words in the command
         if "forward" in command:
