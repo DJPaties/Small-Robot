f6be8db3bb9a1623b5a809d226475790566f0b34
genixRobot <dghaily725@gmail.com>
2024-05-25 09:15:22 +0300

    Update moves.py

--- moves.py
+++ moves.py
@@ -19,14 +19,19 @@ def set_pins(pins, state):
 
 # Function to move forward
 def forward():
-    set_pins(GPIO_PINS["forward"], STATE_ON)
+    i=0.2
+    while i < 1:
+        set_pins(GPIO_PINS["forward"], STATE_ON)
+        i+=0.2
     # set_pins(GPIO_PINS["backward"], STATE_OFF)
 
 # Function to move backward
 def backward():
-    set_pins(GPIO_PINS["backward"], STATE_ON)
-    # set_pins(GPIO_PINS["forward"], STATE_OFF)
-
+    i = 0.2
+    while i <1:
+        set_pins(GPIO_PINS["backward"], STATE_ON)
+        # set_pins(GPIO_PINS["forward"], STATE_OFF)
+        i+=0.2
 # Function to stop
 def stop():
     for pins in GPIO_PINS.values():
@@ -39,41 +44,38 @@ def left():
         subprocess.run(["gpio", "write", str(pin), STATE_ON])
         # time.sleep(0.35)
         # subprocess.run(["gpio", "write", str(pin), STATE_OFF])
-
+    time.sleep(0.4)
 # Function to turn right
 def right():
     local_pins = [28, 27]
     for pin in local_pins:
         subprocess.run(["gpio", "write", str(pin), STATE_ON])
     # set_pins(GPIO_PINS["left"], STATE_OFF)
+    time.sleep(0.4)
 
 # Set GPIO pins as outputs
 for pins in GPIO_PINS.values():
     for pin in pins:
         subprocess.run(["gpio", "mode", str(pin), "out"])
-i = 1
+
 try:
     while True:
         # Accept user input
         # if i>=1:
         direction = input("Enter direction (F: Forward, B: Backward, L: Left, R: Right): ").upper()
-        i=0.2
+        # i=0.2
         
             # Call corresponding function based on user input
         if direction == "F":
-            while i < 1:
                 forward()
-                i+=0.2
+                
         elif direction == "B":
-            while i < 1:
                 backward()
-                i+=0.2
+
         elif direction == "L":
             left()
-            time.sleep(0.5)
         elif direction == "R":
             right()
-            time.sleep(0.5)
         else:
             print("Invalid input. Please enter F, B, L, or R.")
             
